548b4acff4b76f4d896a82f7c362bd1c6eec41aa
haonara <masha-2007.555@mail.ru>
2019-04-18 13:37:53 +1000

    Создан новый проект для дз

---
+++ Homework3/src/ru/java2/dz3/Main.java
@@ -0,0 +1,8 @@
+package ru.java2.dz3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Homework3/src/ru/java2/dz3/PhoneList.java
@@ -0,0 +1,9 @@
+package ru.java2.dz3;
+
+public class PhoneList {
+
+    String surname;
+    int phone;
+
+
+}
