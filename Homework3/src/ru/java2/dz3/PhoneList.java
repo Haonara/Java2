95a3d419d3cc181d548b3b4608886c8de8512fa7
haonara <masha-2007.555@mail.ru>
2019-04-19 15:23:00 +1000

    Выполнено дз

--- Homework3/src/ru/java2/dz3/PhoneList.java
+++ Homework3/src/ru/java2/dz3/PhoneList.java
@@ -1,9 +1,33 @@
 package ru.java2.dz3;
 
+import java.util.*;
+
 public class PhoneList {
 
-    String surname;
-    int phone;
+    public static void main(String[] args) {
+
+        int key;
+        String word;
+        Map<Integer, String> phones = new HashMap<>();
+        phones.put(123456,"Иванов");
+        phones.put(456987,"Петров");
+        phones.put(325698,"Сидоров");
+        phones.put(785412,"Иванов");
+        phones.put(365284,"Козлов");
+
+        for (Map.Entry<Integer,String> ph: phones.entrySet()) {
+            word=ph.getValue();
+            if (word.contains("Петров")){
+                key=ph.getKey();
+                System.out.println(word+ " "+key);
+            }
+
+            
+        }
+
+    }
+
+
 
 
 }
