d2c95728be716812276e34ac85f423f4e99785ad
haonara <masha-2007.555@mail.ru>
2019-04-26 13:37:32 +1000

    Сделано 2 метода. 1 метод вычисляет в побочном потоке значения элементов массива и засекает время выполенния. 2 метод делит массива пополам и в двух побочных потоках вычисляет значения.

--- Homework5/src/ru/java2/dz5/Main.java
+++ Homework5/src/ru/java2/dz5/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //1 method
+        Thread method1=new Thread(new Massiv());
+        method1.start();
+
+        //2 method
+        MyArray method2=new MyArray();
+        method2.myArray();
+
+
+
     }
+
+
 }
