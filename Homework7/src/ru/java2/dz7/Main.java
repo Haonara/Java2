31bdae35dbc0e31ce6e4bd13f52964000e373eee
haonara <masha-2007.555@mail.ru>
2019-05-10 18:11:21 +1000

    Если нет сообщений, то отключается.

--- Homework7/src/ru/java2/dz7/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.java2.dz7;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ Homework7/src/ru/java2/dz7/client/ClientLauncher.java
@@ -0,0 +1,7 @@
+package ru.java2.dz7.client;
+
+public class ClientLauncher {
+    public static void main(String[] args) {
+        ClientWindow clientWindow=new ClientWindow();
+    }
+}
